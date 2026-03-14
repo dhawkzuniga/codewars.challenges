e61e86b315c27ae588149dbc765842dc68e20515
dhawkzuniga <dhawkzuniga@gmail.com>
2026-03-13 22:21:58 -0400

    March 13th challenge

--- march2026/week-2/js/main.js
+++ march2026/week-2/js/main.js
@@ -1,3 +1,11 @@
+// Friday MArch 13th
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+function check(a, x) {
+  let test = (element) => element == x
+  return a.some(test)
+}
+
 // Thursday March 12th Practice, repeating Tuesday's challenge
 function isKiss( wordList ){
   let arr = wordList.split(' ')
