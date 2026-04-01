f27a72658d7e2b31559a4945abc274dbcf501e6d
dhawkzuniga <dhawkzuniga@gmail.com>
2026-04-01 08:50:17 -0400

    April 1 and March 31 challenges

--- april2026/week-1/js/main.js
+++ april2026/week-1/js/main.js
@@ -1,4 +1,23 @@
-//Monday March 31
+//Wednesday April 1
+//make a constructor cuboid with a cube class extension
+class Cuboid {
+  constructor(length, width, height) {
+    Object.assign(this, { length, width, height })
+  }
+  get surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+  }
+  get volume() {
+    return this.length * this.width * this.height
+  }
+}
+class Cube extends Cuboid {
+  constructor(length) {
+    super(length, length, length)
+  }
+}
+
+//Tuesday March 30
 //You're going to implement a simple counter class. The counter will start at zero, and every time its incr method is called, it will increase by 1.
 
 class Counter{
