5509c2ef7d87dd1abf57177f2dbd348f6af50b00
dhawkzuniga <dhawkzuniga@gmail.com>
2026-04-10 07:40:19 -0400

    A challenge to reverse certain words in a string

---
+++ 6kyu/stopSpinningMyWords.js
@@ -0,0 +1,17 @@
+//Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (just like the name of this kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+
+function spinWords(string){
+  let arrOfWords = string.split(' ') //split the string into an array
+  let resultingArray = []
+  
+  for (let i=0; i < arrOfWords.length; i++){ //loop through array
+    if (arrOfWords[i].length >= 5){ //if the word length is bigger than 5
+      resultingArray.push(arrOfWords[i].split('').reverse().join('')) //push it, but reversed to result
+    } else{
+      resultingArray.push(arrOfWords[i]) //otherwise just push it
+    }
+  }
+  
+  return resultingArray.join(' ')
+}
