72ce3b98c1876614af505e00bf0244be3142a1a8
dhawkzuniga <dhawkzuniga@gmail.com>
2026-05-25 12:10:46 -0400

    Count by a number X times

---
+++ 8kyu/countByX.js
@@ -0,0 +1,15 @@
+//Create a function with two arguments that will return an array of the first n multiples of x.
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array
+
+function countBy(x, n) {
+  //return array of x, counted by n times
+  let arr = []
+  for (let i = 1; i<= n; i++){
+    arr.push(x * i)
+  }
+  
+  return arr
+}
